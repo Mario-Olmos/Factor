212732835a4d2286a17198dd651b9c7116680e89
Mario-Olmos <olmosmansomario26@gmail.com>
2025-01-04 10:15:26 +0100

    cambios que no me acuerdo

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   { path: 'theme', component: ThemeComponent , canActivate: [AuthGuard] },
   { path: 'uploadArticle', component: UploadArticleComponent , canActivate: [AuthGuard] },
   { path: 'article/:id', component: ArticuloDetailComponent, canActivate: [AuthGuard] },
-  { path: 'explore', component: FilterPageComponent, canActivate: [AuthGuard] },
+  { path: 'explorador', component: FilterPageComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' } // Ruta predeterminada
 ];
 
